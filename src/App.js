f439046a2fc7aa2ff67b6bf053fb1648552f9a98
Kanaklata91 <kanaklatasingh91@gmail.com>
2022-07-07 09:53:44 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback, useMemo } from 'react';
 import './style.css';
 import Button from './Button';
 
+var functionSet = new Set();
 export default function App() {
   console.log('Counter rendered.');
   const [count1, setCount1] = useState(0);
@@ -15,11 +16,20 @@ export default function App() {
     [count2]
   );
 
+  functionSet.add(useMemoisedCallback1);
+  functionSet.add(useMemoisedCallback2);
+
+  console.log(functionSet);
+
   return (
     <div>
-      {count1} - {count2}
+      {/* {count1} - {count2}
       <Button handleEvent={useMemoisedCallback1} name1="button1"></Button>
-      <Button handleEvent={useMemoisedCallback2} name1="button2"></Button>
+      <Button handleEvent={useMemoisedCallback2} name1="button2"></Button> */}
+      {count1} : {count2}
+      <br /> <br />
+      <button onClick={useMemoisedCallback1}>Button 1</button>
+      <button onClick={useMemoisedCallback2}>Button 2</button>
     </div>
   );
 }
